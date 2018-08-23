e2c524f2fa6138ef0734b914879c4d003d3d2494
Bharat Reddy <bharat8apr@gmail.com>
2018-08-23 23:53:38 +0530

    Stage 11 revised

---
+++ batch.bat
@@ -0,0 +1,7 @@
+load --init /home/bharat/myexpos/expl/samples/numbers.xsm
+load --int=10 /home/bharat/myexpos/spl/spl_progs/haltprog.xsm
+load --exhandler /home/bharat/myexpos/spl/spl_progs/haltprog.xsm
+load --os /home/bharat/myexpos/spl/spl_progs/os_startup.xsm
+load --library /home/bharat/myexpos/expl/library.lib
+load --int=timer /home/bharat/myexpos/spl/spl_progs/sample_timer.xsm
+load --int=7 /home/bharat/myexpos/spl/spl_progs/sample_int7.xsm
