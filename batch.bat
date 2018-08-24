19c7679036982f133f13b5f8ee3cd4b09c5e882d
Bharat Reddy <bharat8apr@gmail.com>
2018-08-24 15:57:31 +0530

    Stage 12 Completed

--- batch.bat
+++ batch.bat
@@ -5,3 +5,4 @@ load --os /home/bharat/myexpos/spl/spl_progs/os_startup.xsm
 load --library /home/bharat/myexpos/expl/library.lib
 load --int=timer /home/bharat/myexpos/spl/spl_progs/sample_timer.xsm
 load --int=7 /home/bharat/myexpos/spl/spl_progs/sample_int7.xsm
+load --idle /home/bharat/myexpos/expl/samples/idle.xsm
