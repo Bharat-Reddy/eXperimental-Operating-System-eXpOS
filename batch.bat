4a7829b9c997638655ec3839f346343e9761ca7d
Bharat Reddy <bharat8apr@gmail.com>
2018-08-27 13:35:04 +0530

    Stage 14 Completed

--- batch.bat
+++ batch.bat
@@ -1,5 +1,5 @@
 load --init /home/bharat/myexpos/expl/samples/numbers.xsm
-load --int=10 /home/bharat/myexpos/spl/spl_progs/haltprog.xsm
+load --int=10 /home/bharat/myexpos/spl/spl_progs/int_10.xsm
 load --exhandler /home/bharat/myexpos/spl/spl_progs/haltprog.xsm
 load --os /home/bharat/myexpos/spl/spl_progs/os_modified.xsm
 load --library /home/bharat/myexpos/expl/library.lib
@@ -7,4 +7,4 @@ load --int=timer /home/bharat/myexpos/spl/spl_progs/sample_timer.xsm
 load --int=7 /home/bharat/myexpos/spl/spl_progs/sample_int7.xsm
 load --idle /home/bharat/myexpos/expl/samples/idle.xsm
 load --module 7 /home/bharat/myexpos/spl/spl_progs/module_7.xsm
-load --exec /home/bharat/myexpos/expl/samples/even.xsm
+load --module 5 /home/bharat/myexpos/spl/spl_progs/module_5.xsm
