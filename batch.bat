72b22ebb97aa86d7a347ee950d4879b2192f9020
Bharat Reddy <bharat8apr@gmail.com>
2018-09-02 10:06:10 +0530

    Stage 15 Completed

--- batch.bat
+++ batch.bat
@@ -8,3 +8,5 @@ load --int=7 /home/bharat/myexpos/spl/spl_progs/sample_int7.xsm
 load --idle /home/bharat/myexpos/expl/samples/idle.xsm
 load --module 7 /home/bharat/myexpos/spl/spl_progs/module_7.xsm
 load --module 5 /home/bharat/myexpos/spl/spl_progs/module_5.xsm
+load --module 0 /home/bharat/myexpos/spl/spl_progs/module_0.xsm
+load --module 4 /home/bharat/myexpos/spl/spl_progs/module_4.xsm
