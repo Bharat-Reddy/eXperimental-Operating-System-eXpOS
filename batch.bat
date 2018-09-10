8143b9030362d2014985f7d32b630b96a9eb04d4
Bharat Reddy <bharat8apr@gmail.com>
2018-09-10 20:12:44 +0530

    Stage 20 Completed!

--- batch.bat
+++ batch.bat
@@ -1,4 +1,4 @@
-load --init /home/bharat/myexpos/expl/samples/shell.xsm
+load --init /home/bharat/myexpos/expl/samples/random.xsm
 load --int=10 /home/bharat/myexpos/spl/spl_progs/int_10.xsm
 load --exhandler /home/bharat/myexpos/spl/spl_progs/exception_handler.xsm
 load --os /home/bharat/myexpos/spl/spl_progs/os_startup.xsm
@@ -9,6 +9,7 @@ load --int=disk /home/bharat/myexpos/spl/spl_progs/disk_interrupt.xsm
 load --int=7 /home/bharat/myexpos/spl/spl_progs/int_7.xsm
 load --int=6 /home/bharat/myexpos/spl/spl_progs/int_6.xsm
 load --int=9 /home/bharat/myexpos/spl/spl_progs/int_9_modified.xsm
+load --int=8 /home/bharat/myexpos/spl/spl_progs/int_8.xsm
 load --idle /home/bharat/myexpos/expl/samples/idle.xsm
 load --module 7 /home/bharat/myexpos/spl/spl_progs/module_7.xsm
 load --module 5 /home/bharat/myexpos/spl/spl_progs/module_5.xsm
