6a20f41363de7e8fd4ef08766927e5cedbdd2dfa
Bharat Reddy <bharat8apr@gmail.com>
2018-10-02 23:00:04 +0530

    Stage 23 Completed

--- batch.bat
+++ batch.bat
@@ -1,4 +1,4 @@
-load --init /home/bharat/myexpos/expl/samples/stage_22.xsm
+load --init /home/bharat/myexpos/expl/samples/shell.xsm
 load --int=10 /home/bharat/myexpos/spl/spl_progs/int_10.xsm
 load --exhandler /home/bharat/myexpos/spl/spl_progs/exception_handler.xsm
 load --os /home/bharat/myexpos/spl/spl_progs/os_startup.xsm
@@ -7,6 +7,7 @@ load --int=timer /home/bharat/myexpos/spl/spl_progs/sample_timer.xsm
 load --int=console /home/bharat/myexpos/spl/spl_progs/console_interrupt.xsm
 load --int=disk /home/bharat/myexpos/spl/spl_progs/disk_interrupt.xsm
 load --int=7 /home/bharat/myexpos/spl/spl_progs/int_7.xsm
+load --int=4 /home/bharat/myexpos/spl/spl_progs/int_4.xsm
 load --int=6 /home/bharat/myexpos/spl/spl_progs/int_6.xsm
 load --int=9 /home/bharat/myexpos/spl/spl_progs/int_9_modified.xsm
 load --int=8 /home/bharat/myexpos/spl/spl_progs/int_8.xsm
@@ -26,3 +27,5 @@ load --exec /home/bharat/myexpos/expl/samples/odd.xsm
 load --exec /home/bharat/myexpos/expl/samples/gcd.xsm
 load --exec /home/bharat/myexpos/expl/samples/sort.xsm
 load --exec /home/bharat/myexpos/expl/samples/stage_22.xsm
+load --exec /home/bharat/myexpos/expl/samples/create.xsm
+load --exec /home/bharat/myexpos/expl/samples/delete.xsm
