fe6029cca9c8f675304a1673f58735e56b91ffb1
Bharat Reddy <bharat8apr@gmail.com>
2019-04-25 13:36:41 +0530

    Final Tweaks

--- batch.bat
+++ batch.bat
@@ -1,4 +1,5 @@
-load --init /home/bharat/myexpos/expl/samples/new_base.xsm
+load --init /home/bharat/myexpos/expl/samples/login.xsm
+load --shell /home/bharat/myexpos/expl/samples/new_shell.xsm
 load --int=10 /home/bharat/myexpos/spl/spl_progs/int_10.xsm
 load --exhandler /home/bharat/myexpos/spl/spl_progs/exception_handler.xsm
 load --os /home/bharat/myexpos/spl/spl_progs/os_startup.xsm
@@ -7,9 +8,18 @@ load --int=timer /home/bharat/myexpos/spl/spl_progs/sample_timer.xsm
 load --int=console /home/bharat/myexpos/spl/spl_progs/console_interrupt.xsm
 load --int=disk /home/bharat/myexpos/spl/spl_progs/disk_interrupt.xsm
 load --int=7 /home/bharat/myexpos/spl/spl_progs/int_7.xsm
+load --int=4 /home/bharat/myexpos/spl/spl_progs/int_4.xsm
+load --int=5 /home/bharat/myexpos/spl/spl_progs/int_5.xsm
 load --int=6 /home/bharat/myexpos/spl/spl_progs/int_6.xsm
 load --int=9 /home/bharat/myexpos/spl/spl_progs/int_9_modified.xsm
 load --int=8 /home/bharat/myexpos/spl/spl_progs/int_8.xsm
+load --int=11 /home/bharat/myexpos/spl/spl_progs/int_11.xsm
+load --int=15 /home/bharat/myexpos/spl/spl_progs/int_15.xsm
+load --int=14 /home/bharat/myexpos/spl/spl_progs/int_14.xsm
+load --int=13 /home/bharat/myexpos/spl/spl_progs/int_13.xsm
+load --int=17 /home/bharat/myexpos/spl/spl_progs/int_17.xsm
+load --int=12 /home/bharat/myexpos/spl/spl_progs/int_12.xsm
+load --int=16 /home/bharat/myexpos/spl/spl_progs/int_16.xsm
 load --idle /home/bharat/myexpos/expl/samples/idle.xsm
 load --module 7 /home/bharat/myexpos/spl/spl_progs/module_7.xsm
 load --module 5 /home/bharat/myexpos/spl/spl_progs/module_5.xsm
@@ -17,4 +27,22 @@ load --module 0 /home/bharat/myexpos/spl/spl_progs/module_0.xsm
 load --module 4 /home/bharat/myexpos/spl/spl_progs/module_4.xsm
 load --module 1 /home/bharat/myexpos/spl/spl_progs/module_1.xsm
 load --module 2 /home/bharat/myexpos/spl/spl_progs/module_2.xsm
-load --exec /home/bharat/myexpos/expl/samples/new_ch.xsm
+load --module 3 /home/bharat/myexpos/spl/spl_progs/module_3.xsm
+load --exec /home/bharat/myexpos/expl/samples/even.xsm
+load --exec /home/bharat/myexpos/expl/samples/odd.xsm
+load --exec /home/bharat/myexpos/expl/samples/gcd.xsm
+load --exec /home/bharat/myexpos/expl/samples/sort.xsm
+load --exec /home/bharat/myexpos/expl/samples/st_22_1.xsm
+load --exec /home/bharat/myexpos/expl/samples/stage_24.xsm
+load --exec /home/bharat/myexpos/expl/samples/create.xsm
+load --exec /home/bharat/myexpos/expl/samples/delete.xsm
+load --data /home/bharat/myexpos/expl/samples/mdata.dat
+load --data /home/bharat/myexpos/expl/samples/numbers.dat
+load --exec /home/bharat/myexpos/expl/samples/parent.xsm
+load --exec /home/bharat/myexpos/expl/samples/child.xsm
+load --exec /home/bharat/myexpos/expl/samples/f_read.xsm
+load --exec /home/bharat/myexpos/expl/samples/m_sort_S.xsm
+load --exec /home/bharat/myexpos/expl/samples/m_sort_C.xsm
+load --exec /home/bharat/myexpos/expl/samples/st_25.xsm
+load --exec /home/bharat/myexpos/expl/samples/st_25-2.xsm
+load --exec /home/bharat/myexpos/expl/samples/f_write.xsm
